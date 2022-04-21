71a0680d6c3df41356dfe6395da3878d11f9af89
Serin-Kim <serin9864@gmail.com>
2022-04-21 16:59:34 +0900

    refactor: props 전달을 destructiong 방식으로 변경

--- frontend/src/js/components/GoodsBlock.jsx
+++ frontend/src/js/components/GoodsBlock.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {eventLabelIcon, launchingLabelIcon} from '../constants/iconPath.js';
 import '../../css/GoodsBlock.css';
 
-function GoodsBlock(props) {
-  const [discountedPrice, regularPrice] = props.price.map(element => element.price);
-  const [eventLabel, launchingLabel] = props.label.map(element => element.exist);
+function GoodsBlock({thumb, name, description, price, label}) {
+  const [discountedPrice, regularPrice] = price.map(element => element.price);
+  const [eventLabel, launchingLabel] = label.map(element => element.exist);
   const eventLabelElement = eventLabel ? (
     <img className="eventLabel" src={eventLabelIcon} alt="eventLabelIcon"></img>
   ) : (
@@ -18,10 +18,10 @@ function GoodsBlock(props) {
 
   return (
     <div className="goodsBlock">
-      <img className="thumb" src={props.thumb} alt="thumb"></img>
+      <img className="thumb" src={thumb} alt="thumb"></img>
       <section className="info">
-        <h4 className="name"> {props.name}</h4>
-        <p className="description">{props.description}</p>
+        <h4 className="name"> {name}</h4>
+        <p className="description">{description}</p>
         <div className="price">
           <p className="discountedPrice">{Number(discountedPrice).toLocaleString('en') + '원'}</p>
           <p className="regularPrice">{Number(regularPrice).toLocaleString('en') + '원'}</p>
