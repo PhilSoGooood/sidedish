5e3ac70b1d57185c0227896b7062d564ca49b482
연 <mogoo22@naver.com>
2022-04-25 22:07:53 +0900

    feat: Header Styled-Component 적용

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -1,47 +1,69 @@
 import React, {useState} from 'react';
+import styled, {css} from 'styled-components';
 import {pageHeaderIcon, searchIcon, userIcon, myCartIcon} from 'constants';
-import {category} from 'data';
-import {NavIcon} from 'components';
-import './Header.css';
+import {NavIcon, Category} from 'components';
+
+const StyledHeader = styled.header`
+  width: 100%;
+  border-bottom: 1px solid;
+  ${({isOpen}) => {
+    return (
+      isOpen &&
+      css`
+        height: 191px;
+      `
+    );
+  }}
+`;
+
+const InnerHeader = styled.div`
+  width: 1280px;
+  height: 83px;
+  display: flex;
+  align-items: baseline;
+  margin: 0 auto;
+  position: relative;
+`;
+
+const BrandHeader = styled.h1`
+  margin: 16px 40px 17px 0px;
+  font-family: 'Outfit';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 40px;
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  width: 128px;
+  height: 32px;
+  margin-left: auto;
+`;
+
+const navIcons = [
+  {id: 1, name: 'searchIcon', src: `${searchIcon}`},
+  {id: 2, name: 'userIcon', src: `${userIcon}`},
+  {id: 3, name: 'myCartIcon', src: `${myCartIcon}`},
+];
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const navIcons = [
-    {name: 'searchIcon', src: `${searchIcon}`},
-    {name: 'userIcon', src: `${userIcon}`},
-    {name: 'myCartIcon', src: `${myCartIcon}`},
-  ];
-
-  const categoryList = category.map(element => (
-    <li key={element.id} className="mainCategory">
-      {element.mainCategory.title}
-      <ul className="subCategoryList">
-        {element.mainCategory.subCategory.map(element => (
-          <li key={element.id} className="subCategory" style={{display: isOpen ? 'flex' : 'none'}}>
-            {element.title}
-          </li>
-        ))}
-      </ul>
-    </li>
-  ));
-
   return (
-    <header className={'header ' + (isOpen ? 'isOpen' : '')}>
-      <div className="innerHeader">
-        <h1 className="brandHeader">
+    <StyledHeader isOpen={isOpen}>
+      <InnerHeader>
+        <BrandHeader>
           <img src={pageHeaderIcon} alt="pageHeaderIcon"></img>
-        </h1>
-        <div className="category" onMouseOver={() => setIsOpen(true)} onMouseOut={() => setIsOpen(false)}>
-          <ul className="categoryList">{categoryList}</ul>
-        </div>
-        <nav className="navBar">
-          {navIcons.map(nav => (
-            <NavIcon name={nav.name} src={nav.src} />
+        </BrandHeader>
+        <Category isOpen={isOpen} setIsOpen={setIsOpen} />
+        <NavBar>
+          {navIcons.map(({id, name, src}) => (
+            <NavIcon key={id} name={name} src={src} />
           ))}
-        </nav>
-      </div>
-    </header>
+        </NavBar>
+      </InnerHeader>
+    </StyledHeader>
   );
 }
 
