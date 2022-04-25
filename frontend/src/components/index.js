b76b8f686feddf62ba98760c43f36c03d069ff7c
연 <mogoo22@naver.com>
2022-04-25 23:16:07 +0900

    remove: SideDish Component삭제
    return문 내부에서 Element 이외의 요소(map 메소드)를 상위 컴포넌트에서 사용함

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,5 +3,4 @@ export * from './TabList/TabList';
 export * from './Category/Category';
 export * from './SideDishContentsList/SideDishContentsList';
 export * from './Slider/Slider';
-export * from './SideDish/SideDish';
 export * from './GoodsBlock/GoodsBlock';
