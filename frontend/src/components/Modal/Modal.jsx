61287d71e7da30b99a2eb1a81d8d756bc4bcc9e0
연 <mogoo22@naver.com>
2022-04-28 23:27:08 +0900

    feat: 모달창 렌더 및 post 요청 기능 구현
    모달창 마크업, 주문하기 기능 구현
    
    Co-authored-by: Serin Kim  <Serin-Kim@users.noreply.github.com>

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-import React, {useState, useEffect} from "react";
-=======
 import React, {useState, useEffect, useCallback} from "react";
->>>>>>> dev-FE
 import {fetchData, postData} from "utils/utils";
+import {minusIcon, plusIcon, eventLabelIcon, launchingLabelIcon} from "constants";
 import {serverURL} from "constants/urlPath";
 import {
   DarkBackground,
@@ -20,21 +17,26 @@ import {
 } from "./Modal.styled";
 
 function Modal({visible, onClose, productId}) {
-  //const [goodsData, setGoodsData] = useState([]);
-  const [orderInfo, setOrderInfo] = useState({id: 0, quantity: 0, price: 0});
+  const [goodsData, setGoodsData] = useState();
   const [orderState, setOrderState] = useState(false);
   const [orderSuccess, setOrderSuccess] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [orderInfo, setOrderInfo] = useState({productId, quantity, price: totalPrice});
 
   const fetchAPI = useCallback(async () => {
     if (!productId) return;
     const data = await fetchData(`${serverURL}/${productId}`);
-    console.log(data);
-    //setGoodsData(data)
+    setGoodsData(data);
+    const price = data.price * (1 - data.discountRate);
+    setPrice(price);
+    setTotalPrice(price);
   }, [productId]);
 
   const handleOrder = () => {
     setOrderState(true);
-    setOrderInfo({...orderInfo, id: productId, quantity: 1, price: 100});
+    setOrderInfo({...orderInfo, productId, quantity, price: totalPrice});
   };
 
   const postAPI = useCallback(async () => {
@@ -52,6 +54,10 @@ function Modal({visible, onClose, productId}) {
     postAPI();
   }, [postAPI]);
 
+  useEffect(() => {
+    setTotalPrice(price * quantity);
+  }, [quantity]);
+
   if (!visible) return;
   return (
     <DarkBackground>
@@ -59,19 +65,73 @@ function Modal({visible, onClose, productId}) {
         {orderSuccess ? (
           <SuccessOrder>주문이 완료되었습니다. 잠시후 메인 페이지로 이동합니다.</SuccessOrder>
         ) : (
-          <>
-            <CloseButton onClick={onClose}>닫기</CloseButton>
-            <InnerModalBlock>
-              <ProductImages>
-                <MainThumb></MainThumb>
-                <SubThumb></SubThumb>
-              </ProductImages>
-              <Product>
-                <ProductInfo></ProductInfo>
-                <OrderButton onClick={handleOrder}>주문하기</OrderButton>
-              </Product>
-            </InnerModalBlock>
-          </>
+          goodsData && (
+            <>
+              <CloseButton onClick={onClose}>닫기</CloseButton>
+              <InnerModalBlock>
+                <ProductImages>
+                  <MainThumb src={goodsData.image[0].imagePath}></MainThumb>
+                  <SubThumb></SubThumb>
+                </ProductImages>
+                <Product>
+                  <ProductInfo>
+                    <p className="productName">{goodsData.productName}</p>
+                    <div className="priceBox">
+                      <div className="eventBadge">
+                        {goodsData.eventBadge !== "none" && (
+                          <img
+                            className="eventBadge"
+                            src={goodsData.eventBadge === "event" ? eventLabelIcon : launchingLabelIcon}
+                            alt="eventBadgeIcon"
+                          ></img>
+                        )}
+                      </div>
+
+                      <div className="productPrice">
+                        <p className="discountedPrice">
+                          {Number(goodsData.price * (1 - goodsData.discountRate)).toLocaleString("en") + "원"}
+                        </p>
+                        {goodsData.discountRate !== 0 && (
+                          <p className="regularPrice">
+                            {Number(goodsData.price).toLocaleString("en") + "원"}
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                    <div className="orderBox">
+                      <div className="orderCountBox">
+                        <button>
+                          <img
+                            className="minusOrderIcon"
+                            src={minusIcon}
+                            alt="orderCountIcon"
+                            onClick={() => {
+                              if (quantity === 1) return;
+                              setQuantity(quantity - 1);
+                            }}
+                          ></img>
+                        </button>
+                        <p className="orderCount">{quantity}</p>
+                        <button>
+                          <img
+                            className="plusOrderIcon"
+                            src={plusIcon}
+                            alt="orderCountIcon"
+                            onClick={() => setQuantity(quantity + 1)}
+                          ></img>
+                        </button>
+                      </div>
+                      <div className="totalPriceBox">
+                        <p className="totalPriceTitle">총 주문금액</p>
+                        <p className="totalPrice">{Number(totalPrice).toLocaleString("en") + "원"}</p>
+                      </div>
+                    </div>
+                  </ProductInfo>
+                  <OrderButton onClick={handleOrder}>주문하기</OrderButton>
+                </Product>
+              </InnerModalBlock>
+            </>
+          )
         )}
       </ModalBlock>
     </DarkBackground>
